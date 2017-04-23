b2f2c8983cde3cdc7adeeba1a3b4b70c8c40a5a5
hub33k <hub33k@yahoo.com>
2017-04-23 13:46:19 +0200

    Add basic chat logic

--- client.js
+++ client.js
@@ -1,4 +1,29 @@
 const io = require('socket.io-client');
 const socket = io('http://localhost:3000');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-console.log('Client conneted');
+function clearPrompt() {
+    process.stdout.cursorTo(0);
+    process.stdout.clearLine();
+}
+
+socket.on('message', (msg) => {
+    clearPrompt();
+    console.log(`>> ${msg}`);
+    readline.prompt();
+});
+
+console.log('Client connected.');
+
+// get message from client
+readline.on('line', (line) => {
+    if (line.trim()) {
+        socket.emit('message', line);
+    }
+    readline.prompt();
+});
+
+readline.prompt();
--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const server = io(); // socket
 
 server.on('connection', (client) => {
     console.log(`Hello, your id: ${client.id}`);
+
+    client.on('message', (msg) => {
+        console.log(msg);
+        client.broadcast.emit('message', msg); // send msg to all clients
+    });
 });
+
+console.log('Server started!');
 
 server.listen(3000);
