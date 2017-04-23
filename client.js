c9d1ed89b46062a776f2dbdce4e341f0a7602e0e
hub33k <hub33k@yahoo.com>
2017-04-23 14:19:43 +0200

    Get client url from cmd line

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client');
-const socket = io('http://localhost:3000');
+const socket = io(`http://${process.argv[2] || 'localhost' }:3000`);
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
