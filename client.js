72c6316e6342f5489731e5532ba787f68fc4e1c0
hub33k <hub33k@yahoo.com>
2017-04-23 12:25:57 +0200

    Create basic connection between server and client

---
+++ client.js
@@ -0,0 +1,4 @@
+const io = require('socket.io-client');
+const socket = io('http://localhost:3000');
+
+console.log('Client conneted');
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const io = require('socket.io');
 const server = io(); // socket
 
+server.on('connection', (client) => {
+    console.log(`Hello, your id: ${client.id}`);
+});
+
 server.listen(3000);
