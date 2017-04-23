6a440f770d0d9fc8e0adea8bf1d0a3b3a8dee749
hub33k <hub33k@yahoo.com>
2017-04-23 16:07:08 +0200

    Add commands to chat

--- client.js
+++ client.js
@@ -10,6 +10,28 @@ function clearPrompt() {
     process.stdout.clearLine();
 }
 
+socket.on('register', (registerSuccess) => {
+    clearPrompt();
+    if (registerSuccess) {
+        console.log('>> Registration success!');
+    } else {
+        console.log('>> Registration failed!');
+    }
+    readline.prompt();
+});
+
+socket.on('login', (username) => {
+    clearPrompt();
+    if (username) {
+        LOGGED_USER = username;
+        readline.setPrompt(`${username}: `);
+    } else {
+        console.log('>> Login failed!');
+    }
+
+    readline.prompt();
+});
+
 socket.on('message', (msg) => {
     clearPrompt();
     console.log(`>> ${msg}`);
@@ -18,11 +40,38 @@ socket.on('message', (msg) => {
 
 console.log('Client connected.');
 
+let LOGGED_USER = '';
+
 // get message from client
 readline.on('line', (line) => {
-    if (line.trim()) {
+    const lineArgs = line.split(/\s+/);
+    const firstWorld = lineArgs[0];
+
+    if (firstWorld === '/exit' || firstWorld === '/q') {
+        readline.close();
+        process.exit(0)
+    } else if (firstWorld === '/register' || firstWorld === '/r') {
+        if (lineArgs.length >= 3) {
+            socket.emit('register', {
+                username: lineArgs[1],
+                password: lineArgs[2],
+            });
+        }
+    } else if (firstWorld === '/login' || firstWorld === '/l') {
+        if (lineArgs.length >= 3) {
+            socket.emit('login', {
+                username: lineArgs[1],
+                password: lineArgs[2],
+            });
+        }
+    } else if (firstWorld === '/logout') {
+        socket.emit('logout', LOGGED_USER);
+        LOGGED_USER = '';
+        readline.setPrompt('> ');
+    } else if (line.trim()) {
         socket.emit('message', line);
     }
+
     readline.prompt();
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const io = require('socket.io');
 const server = io(); // socket
 
+const USERS = {};
+
 server.on('connection', (client) => {
     console.log(`Hello, your id: ${client.id}`);
 
@@ -8,8 +10,33 @@ server.on('connection', (client) => {
         console.log(msg);
         client.broadcast.emit('message', msg); // send msg to all clients
     });
-});
 
-console.log('Server started!');
+    client.on('register', (userObject) => {
+        if (USERS[userObject.username]) {
+            client.emit('register', false);
+        } else {
+            USERS[userObject.username] = {
+                password: userObject.password,
+                logged_in: false,
+            };
+            client.emit('register', true);
+        }
+    });
+
+    client.on('login', (userObject) => {
+        const user = USERS[userObject.username];
+
+        if (user.password === userObject.password) {
+            user.logged_in = true;
+            client.emit('login', userObject.username);
+        } else {
+            client.emit('login', false);
+        }
+    });
+
+    client.on('logout', (username) => {
+        USERS[username].logged_in = false;
+    });
+});
 
 server.listen(3000);
